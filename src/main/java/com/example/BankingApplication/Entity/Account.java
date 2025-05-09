59aaf4fe9b0841cc16f2e616c99aaa3e62a0d711
SujalPrajapati2006 <prajapatisujal123456@gmail.com>
2025-05-09 12:56:21 +0530

    Update Account.java

--- src/main/java/com/example/BankingApplication/Entity/Account.java
+++ src/main/java/com/example/BankingApplication/Entity/Account.java
@@ -6,11 +6,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
-//import org.springframework.data.annotation.Id;
 
+@Entity
 @Getter
 @Setter
-@Entity
 public class Account {
 
     @Id
