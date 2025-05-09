7c6a18b839143ef8b91ec330d1a294287dc7a387
SujalPrajapati2006 <prajapatisujal123456@gmail.com>
2025-05-09 13:00:36 +0530

    Update AccountController.java

--- src/main/java/com/example/BankingApplication/Controller/AccountController.java
+++ src/main/java/com/example/BankingApplication/Controller/AccountController.java
@@ -1,48 +1,54 @@
 package com.example.BankingApplication.Controller;
 
-import com.example.BankingApplication.Entity.Account;
-import com.example.BankingApplication.service.AccountService;
+import com.example.PRECTICE.Entity.Account;
+import com.example.PRECTICE.Service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Map;
+import java.util.List;
+import java.util.Optional;
+
 @RestController
-@RequestMapping("/api/accounts")
+@RequestMapping("/account")
 public class AccountController {
 
     @Autowired
     private AccountService accountService;
 
     @PostMapping
-    public Account createAccount(@RequestBody Account account) {
-        return accountService.createAccount(account);
+    public Account createAccount(@RequestBody Account account){
+         return accountService.createAccount(account);
+    }
+
+    @GetMapping
+    public List<Account> getAccount(){
+         return accountService.getAccount();
     }
 
     @GetMapping("/{id}")
-    public Account getAccount(@PathVariable Long id) {
-        return accountService.getAccount(id).orElseThrow(() -> new RuntimeException("Account not found"));
+    public Optional<Account> getAccountById(@PathVariable Long id){
+         return accountService.getAccountById(id);
     }
 
     @PostMapping("/{id}/deposit")
-    public Account deposit(@PathVariable Long id, @RequestBody Map<String, Double> request) {
-        Double amount = request.get("amount");
-        return accountService.deposit(id, amount);
+    public Account deposit(@PathVariable Long id, @RequestBody Map<String, Double> request){
+       Double amount = request.get("amount");
+       return accountService.deposit(id,amount);
     }
 
-    @PostMapping("/{id}/withdraw")
-    public Account withdraw(@PathVariable Long id, @RequestBody Map<String, Double> request) {
+    @PostMapping("/{id}/withdarw")
+    public Account withdraw(@PathVariable Long id,@RequestBody Map<String,Double> request){
         Double amount = request.get("amount");
-        return accountService.withdraw(id, amount);
+        return accountService.withdraw(id,amount);
     }
 
     @DeleteMapping("/{id}")
-    public String deleteAccount(@PathVariable Long id){
-         accountService.deleteAccount(id);
-         return  "Account with ID " + id + " has been deleted. ";
+    public void delete(@PathVariable  Long id){
+         accountService.delete(id);
     }
 
     @PutMapping("/{id}")
-     public Account updateAccount(@PathVariable Long id,@RequestBody Account account){
-         return accountService.updateAccount(id,account);
+    public Account updateAccount(@PathVariable Long id,@RequestBody Account account){
+         return accountService.updateAccount(id, account);
     }
 }
